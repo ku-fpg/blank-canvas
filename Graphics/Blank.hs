f02536b963558f6e57b78f52b532e058856dff23
Andrew Farmer <anfarmer@ku.edu>
2012-04-13 16:51:17 -0500

    Random stuff

--- Graphics/Blank.hs
+++ Graphics/Blank.hs
@@ -1,26 +1,26 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 
 module Graphics.Blank where
 
 import Control.Concurrent
 import Web.Scotty
 import Network.Wai.Middleware.RequestLogger
+import Network.Wai.Middleware.Static
 
+import Data.Aeson.TH (deriveJSON)
 
 data Canvas = Canvas
+
+data Command = NoOp -- | ...
+$(deriveJSON Prelude.id ''Command)
 
 blankCanvas :: Int -> (Canvas -> IO ()) -> IO ()
 blankCanvas port actions = do
    var <- newMVar "/* empty */"
    scotty port $ do
         middleware logStdoutDev
---        middleware static
+        middleware $ staticRoot "static"
 
-        get "/" $ text "foobar"
+        get "/" $ file "static/index.html"
 
-
-
-
-
-
-
+        get "/poll" $ json [NoOp]
