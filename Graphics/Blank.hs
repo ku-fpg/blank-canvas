716314454e020ac425f73884d160506ca7319663
Andy Gill <andygill@ku.edu>
2012-04-14 19:56:55 -0500

    Major refactor to allow for input from canvas by using Monads.
    The demo is the same, but the internals are different.

--- Graphics/Blank.hs
+++ Graphics/Blank.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell, GADTs, KindSignatures #-}
 
 module Graphics.Blank where
 
@@ -11,8 +11,10 @@ import qualified Data.Text.Lazy as T
 import Numeric
 
 import Data.Aeson.TH (deriveJSON)
+import Data.Aeson (Value)
 
-data Canvas = Canvas Int Int (MVar String)
+
+data Context = Context Int Int (MVar String)
 
 type Dimensions = (Int, Int)
 
@@ -20,13 +22,36 @@ type Point = (Int, Int)
 data JsCommand = NoOp | DrawLine Point Point -- | ...
 $(deriveJSON Prelude.id ''JsCommand)
 
-blankCanvas :: Int -> (Canvas -> IO ()) -> IO ()
+data JsEvent = JsEvent
+        { jsCode  :: Int
+        , jsMouse :: (Int,Int)
+        }
+        deriving (Show)
+
+data EventName
+        -- Keys
+        = KeyPress
+        | KeyDown
+        | KeyUp
+        -- Mouse
+        | MouseDown
+        | MouseEnter
+        | MouseMove
+        | MouseOut
+        | MouseOver
+        | MouseUp
+        deriving (Show)
+
+
+-- $(deriveJSON Prelude.id ''JsEvent)
+
+blankCanvas :: Int -> (Context -> IO ()) -> IO ()
 blankCanvas port actions = do
    picture <- newEmptyMVar
 
    -- perform the canvas writing actions
    -- in worker thread.
-   forkIO $ actions (Canvas 800 600 picture)
+   forkIO $ actions (Context 800 600 picture)
 
    dims <- newMVar (100 :: Int, 100 :: Int)
 
@@ -41,12 +66,20 @@ blankCanvas port actions = do
             liftIO $ modifyMVar_ dims (const (return req))
             json ()
 -}
+        post "/event" $ do
+            req <- jsonData
+--            liftIO $ print (req  :: JsEvent) -- modifyMVar_ dims (const (return req))
+
+            liftIO $ print (req  :: Value) -- modifyMVar_ dims (const (return req))
+            json ()
+
         get "/canvas" $ do
             header "Cache-Control" "max-age=0, no-cache, private, no-store, must-revalidate"
             -- do something and return a new list of commands to the client
             res <- liftIO $ tryTakeMVar picture
             case res of
-              Just js -> text ("var c = getContext();" `T.append` T.pack js)
+              Just js -> do
+                      text ("var c = getContext();" `T.append` T.pack js)
               Nothing -> text (T.pack "redraw();")
 
 
@@ -56,48 +89,96 @@ blankCanvas port actions = do
 --                res <- tryTakeMVar
             json [NoOp,DrawLine (5,5) (50,50)]
 
-width :: Canvas -> Float
-width (Canvas w _ _) = fromIntegral w
+width :: Context -> Float
+width (Context w _ _) = fromIntegral w
 
-height :: Canvas -> Float
-height (Canvas _ h _) = fromIntegral h
+height :: Context -> Float
+height (Context _ h _) = fromIntegral h
 
-send :: Canvas -> [Command] -> IO ()
-send (Canvas _ _ var) commands = do
+send :: Context -> Canvas () -> IO ()
+send (Context _ _ var) commands = send' commands id
+  where
+      send' :: Canvas a -> (String -> String) -> IO a
 
-    putMVar var $ unlines $
-        [ "var c = getContext(); "
-        ] ++ map show commands
-          ++ [ "redraw();" ]
+      send' (Bind (Return a) k)    cmds = send' (k a) cmds
+      send' (Bind (Bind m k1) k2)  cmds = send' (Bind m (\ r -> Bind (k1 r) k2)) cmds
+      send' (Bind (Command cmd) k) cmds = send' (k ()) (cmds . shows cmd)
+      send' (Bind other k)         cmds = do
+              res <- send' other cmds
+              send' (k res) id
 
-    return ()
+{-
+      send' (Get a)                cmds = do
+              -- Hack
+              () <- send' (Return a) cmds
+              return $ undefined
+-}
+      send' (Return a)              cmds = do
+              putMVar var $ "var c = getContext(); " ++ cmds "redraw();"
+              return a
+      send' other                  cmds = send' (Bind other Return) cmds
 
-wait :: Canvas -> IO ()
-wait (Canvas _ _ var)  = do
+{-
+      send' (Get name)             cmds = do
+              -- send the commands
+              return undefined
+-}
+--      send' (Command draw) todo = todo ++ show commands
+{-
 
-    putMVar var $ unlines $
-        [ ]
 
-    return ()
 
+
+-}
+--    return ()
+
+
+data StateOfInput = StateOfInput
+        { mousePos :: Maybe (Int,Int)
+        , mousePress :: Bool
+        }
+
+
+{-
+   Browser
+   Keyboard
+   Mouse
+     - down/up
+   Touch
+ -}
+
+data Canvas :: * -> * where
+        Command :: Command                       -> Canvas ()
+        Bind    :: Canvas a -> (a -> Canvas b)   -> Canvas b
+        Return  :: a                             -> Canvas a
+        Get     :: EventName                     -> Canvas JsEvent
+
+
+instance Monad Canvas where
+        return = Return
+        (>>=) = Bind
+
+
+-- HTML5 Canvas assignments: FillStyle, LineWidth, MiterLimit, StrokeStyle
 data Command
+        -- regular HTML5 canvas commands
         = BeginPath
-        | ClearRect Float Float Float Float
+        | ClearRect (Float,Float,Float,Float)
         | ClosePath
         | Fill
         | FillStyle String
-        | LineTo Float Float
+        | LineTo (Float,Float)
         | LineWidth Float
         | MiterLimit Float
-        | MoveTo Float Float
+        | MoveTo (Float,Float)
         | Restore
         | Rotate Float
-        | Scale Float Float
+        | Scale (Float,Float)
         | Save
         | Stroke
         | StrokeStyle String
-        | Transform Float Float Float Float Float Float
-        | Translate Float Float
+        | Transform (Float,Float,Float,Float,Float,Float)
+        | Translate (Float,Float)
 
 rgba :: (Int,Int,Int,Int) -> String
 rgba (r,g,b,a) = "rgba(" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "," ++ show a ++ ")"
@@ -105,9 +186,27 @@ rgba (r,g,b,a) = "rgba(" ++ show r ++ "," ++ show g ++ "," ++ show b ++ "," ++ s
 showJ a = showFFloat (Just 3) a ""
 
 instance Show Command where
+  show BeginPath = "c.beginPath();"
+  show (ClearRect (a1,a2,a3,a4)) = "c.clearRect(" ++ showJ a1 ++ "," ++ showJ a2 ++ "," ++ showJ a3 ++ "," ++ showJ a4 ++ ");"
+  show ClosePath = "c.closePath();"
+  show Fill = "c.fill();"
+  show (FillStyle (a1)) = "c.fillStyle = (" ++ show a1 ++ ");"
+  show (LineTo (a1,a2)) = "c.lineTo(" ++ showJ a1 ++ "," ++ showJ a2 ++ ");"
+  show (LineWidth (a1)) = "c.lineWidth = (" ++ showJ a1 ++ ");"
+  show (MiterLimit (a1)) = "c.miterLimit = (" ++ showJ a1 ++ ");"
+  show (MoveTo (a1,a2)) = "c.moveTo(" ++ showJ a1 ++ "," ++ showJ a2 ++ ");"
+  show Restore = "c.restore();"
+  show (Rotate (a1)) = "c.rotate(" ++ showJ a1 ++ ");"
+  show (Scale (a1,a2)) = "c.scale(" ++ showJ a1 ++ "," ++ showJ a2 ++ ");"
+  show Save = "c.save();"
+  show Stroke = "c.stroke();"
+  show (StrokeStyle (a1)) = "c.strokeStyle = (" ++ show a1 ++ ");"
+  show (Transform (a1,a2,a3,a4,a5,a6)) = "c.transform(" ++ showJ a1 ++ "," ++ showJ a2 ++ "," ++ showJ a3 ++ "," ++ showJ a4 ++ "," ++ showJ a5 ++ "," ++ showJ a6 ++ ");"
+  show (Translate (a1,a2)) = "c.translate(" ++ showJ a1 ++ "," ++ showJ a2 ++ ");"
+{-
   show (BeginPath)             = "c.beginPath();"
   show (ClosePath)             = "c.closePath();"
-  show (ClearRect a b c d)     = "c.clearRect(" ++ showJ a ++ "," ++ showJ b ++ "," ++ showJ c ++ "," ++ showJ d ++ ");"
+--  show (ClearRect a b c d)     = "c.clearRect(" ++ showJ a ++ "," ++ showJ b ++ "," ++ showJ c ++ "," ++ showJ d ++ ");"
   show (Fill)                  = "c.fill();"
   show (FillStyle any)         = "c.fillStyle = " ++ show any ++ ";"
   show (LineTo a b)            = "c.lineTo(" ++ showJ a ++ "," ++ showJ b ++ ");"
@@ -122,34 +221,65 @@ instance Show Command where
   show (StrokeStyle any)       = "c.strokeStyle = " ++ show any ++ ";"
   show (Transform a b c d e f) = "c.transform(" ++ showJ a ++ "," ++ showJ b ++ "," ++ showJ c ++ "," ++ showJ d ++ "," ++ showJ e ++ "," ++ showJ f ++ ");"
   show (Translate a b)         = "c.translate(" ++ showJ a ++ "," ++ showJ b ++ ");"
-
+-}
 -- And our Deep DSL.
 
-beginPath = BeginPath
-closePath = ClosePath
-clearRect (a,b,c,d) = ClearRect a b c d
+--keypress :: (Int -> IO ()) -> Command
+--keypress f =
 
-fill = Fill
+-----------------------------------------------------------------
 
-fillStyle str = FillStyle str
+-- get :: EventName -> Command
 
-lineTo (a,b) = LineTo a b
+-----------------------------------------------------------------
 
-lineWidth w = LineWidth w
+beginPath :: Canvas ()
+beginPath = Command BeginPath
 
-miterLimit :: Float -> Command
-miterLimit f = MiterLimit f
+clearRect :: (Float,Float,Float,Float) -> Canvas ()
+clearRect = Command . ClearRect
 
-moveTo (a,b) = MoveTo a b
+closePath :: Canvas ()
+closePath = Command ClosePath
 
-strokeStyle :: String -> Command
-strokeStyle str = StrokeStyle str
+fill :: Canvas ()
+fill = Command Fill
 
-restore = Restore
-rotate = Rotate
-scale (a,b) = Scale a b
-save = Save
-stroke = Stroke
-transform (a,b,c,d,e,f) = Transform a b c d e f
-translate (a,b) = Translate a b
+fillStyle :: String -> Canvas ()
+fillStyle = Command . FillStyle
 
+lineTo :: (Float,Float) -> Canvas ()
+lineTo = Command . LineTo
+
+lineWidth :: Float -> Canvas ()
+lineWidth = Command . LineWidth
+
+miterLimit :: Float -> Canvas ()
+miterLimit = Command . MiterLimit
+
+moveTo :: (Float,Float) -> Canvas ()
+moveTo = Command . MoveTo
+
+restore :: Canvas ()
+restore = Command Restore
+
+rotate :: Float -> Canvas ()
+rotate = Command . Rotate
+
+scale :: (Float,Float) -> Canvas ()
+scale = Command . Scale
+
+save :: Canvas ()
+save = Command Save
+
+stroke :: Canvas ()
+stroke = Command Stroke
+
+strokeStyle :: String -> Canvas ()
+strokeStyle = Command . StrokeStyle
+
+transform :: (Float,Float,Float,Float,Float,Float) -> Canvas ()
+transform = Command . Transform
+
+translate :: (Float,Float) -> Canvas ()
+translate = Command . Translate
--- Main.hs
+++ Main.hs
@@ -5,22 +5,19 @@ main = blankCanvas 3000 $ \ canvas -> loop canvas (0 :: Float)
  where
 --    loop canvas n | n > 2 = wait canvas
     loop canvas n = do
-        send canvas $
-                [ clearRect (0,0, width canvas, height canvas)
-                , beginPath
-                ] ++
-                concat [
-                [ save
-                , translate (fromIntegral x * 80,height canvas / 2)
-                , rotate (pi * n)
-                , beginPath
-                , moveTo(-100,-100)
-                , lineTo(100,100)
-                , lineWidth (fromIntegral x)
-                , strokeStyle $ rgba (x * 20,255 - x * 20,128,255) -- fromIntegral x / 10,1 - fromIntegral x / 10,0.5,1)
-                , stroke
-                , restore
-                ] | x <- [1..10]
-                ]
+        send canvas $ do
+                clearRect (0,0, width canvas, height canvas)
+                beginPath
+                sequence_ [ do
+                  save
+                  translate (fromIntegral x * 80,height canvas / 2)
+                  rotate (pi * n)
+                  beginPath
+                  moveTo(-100,-100)
+                  lineTo(100,100)
+                  lineWidth (fromIntegral x)
+                  strokeStyle $ rgba (x * 20,255 - x * 20,128,255) -- fromIntegral x / 10,1 - fromIntegral x / 10,0.5,1)
+                  stroke
+                  restore | x <- [1..10] ]
         loop canvas (n + 0.01) -- if n > 0.99 then 0 else n + 0.01)
 
