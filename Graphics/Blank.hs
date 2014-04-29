ef5d2f593f11ad3cfef86ad6f691a8f273bc9958
Andy Gill <andygill@ku.edu>
2014-04-29 12:05:22 -0500

    Adding flag for remote access

--- Graphics/Blank.hs
+++ Graphics/Blank.hs
@@ -237,7 +237,8 @@ getUniq = do
 data Options = Options 
         { port   :: Int            -- ^ which port do we issue the blank canvas using
         , events :: [EventName]    -- ^ which events does the canvas listen to
-        , debug  :: Bool           -- ^ turn on debugging
+        , debug  :: Bool           -- ^ turn on debugging (default False)
+        , remote :: Bool           -- ^ turn on remote access (default False)
         }
         
 instance Num Options where
@@ -246,5 +247,5 @@ instance Num Options where
     (*) = error "no arithmetic for Blank Canvas Options"
     abs = error "no arithmetic for Blank Canvas Options"
     signum = error "no arithmetic for Blank Canvas Options"
-    fromInteger n = Options { port = fromInteger n, events = [], debug = False }
+    fromInteger n = Options { port = fromInteger n, events = [], debug = False, remote = False }
 
