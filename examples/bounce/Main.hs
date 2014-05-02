7530c0ca2c30f49170d3515b2fd2092a9a922929
Andy Gill <andygill@ku.edu>
2014-05-02 08:46:56 -0500

    Bouncing ball example

---
+++ examples/bounce/Main.hs
@@ -0,0 +1,70 @@
+module Main where
+
+import Graphics.Blank
+import Data.Time.Clock
+import Control.Concurrent
+import Control.Concurrent.STM
+
+main :: IO ()
+main = blankCanvas 3000 { events = ["mousedown"] } $ go
+
+type Ball a = ((Float,Float),Float,a)
+
+type Color = String
+
+epoch :: [Ball ()]
+epoch = []
+
+type State = ([Ball Color])
+
+
+showBall :: (Float,Float) -> String -> Canvas ()
+showBall (x,y) col = do
+	save()
+	translate (x,y)
+
+        beginPath()
+--        scale (1,0.9)
+        fillStyle col
+        arc(0, 0, 50, 0, pi*2, False)
+        closePath()
+        fill()
+        
+	restore()
+
+moveBall :: Ball a -> Ball a
+moveBall ((x,y),d,a) = ((x,y+d),d+0.5,a)
+
+go context = do
+
+     (width,height) <- send context size
+     print (width,height)
+
+     let bounce :: Ball a -> Ball a
+         bounce ((x,y),d,a)
+            | y + 25 >= height && d > 0 = ((x,y),-d*0.95,a)
+            | otherwise         = ((x,y),d,a)
+
+     let loop (balls,cols) = do
+--             print state
+
+             send context $ do
+                clearCanvas
+                sequence_
+                     [ showBall xy col
+                     | (xy,_,col) <- balls
+                     ]
+             threadDelay (20 * 1000)	                   
+
+             es <- flush context
+             if (null es) then return () else print es
+
+             let newBalls = [ ((fromIntegral x,fromIntegral y),0,head cols) 
+                            | NamedEvent _ (Event _ (Just (x,y))) <- es 
+                            ]
+                      
+             loop (map bounce $ map moveBall $ balls ++ newBalls, tail cols)
+
+
+     loop ([((100,100),0,"blue")],cycle ["red","blue","green","orange","cyan"])
+      
