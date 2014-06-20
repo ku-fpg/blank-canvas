c9023a19b2ce6d7f0b3c2e5d0d1ac19fc8ebf67b
Andy Gill <andygill@ku.edu>
2014-06-20 15:02:17 -0500

    Adding better logo

--- wiki-suite/Favicon.hs
+++ wiki-suite/Favicon.hs
@@ -8,50 +8,28 @@ main = blankCanvas 3000 $ \ context -> do -- start blank canvas on port 3000
         send context $ do                 -- send commands to this specific context
 
              beginPath()
-             moveTo(16,1)
-             lineTo(8,32)
-             lineWidth 2
-             strokeStyle "blue"
-             lineCap "round"
-             stroke()                     
+	     let relPath x xs = do
+	     	      beginPath()
+		      moveTo x
+		      sequence_ [ lineTo p | ss <- drop 2 $ scanl (flip (:)) [] (x:xs)
+		      		  	   , let p = (sum (map fst ss), sum (map snd ss))
+					   ]
+		      closePath()
+		      
+             translate(height context * 0.07,height context * 0.8)
+	     scale (height context / 20,-height context / 20)
 
-             beginPath()
-             moveTo(16,1)
-             lineTo(16+8,32)
-             lineWidth 2
-             strokeStyle "blue"
-             lineCap "round"
-             stroke()                     
+	     let shape x xs col = do
+	     	     relPath x xs
+		     lineWidth 0.5
+		     fillStyle col
+		     lineCap "round"
+		     fill()                     
 
-	     beginPath()
-	     rect(0.5,1.5,31,22)
-	     fillStyle "white"
-	     fill()
-	     lineWidth 1
-	     strokeStyle "red"
-	     stroke()
-
-             beginPath()
-             moveTo(4,25.5)
-             lineTo(32-4,25.5)
-             lineWidth 3
-             strokeStyle "black"
-             lineCap "butt"
-             stroke()                     
-
-             lineWidth 1.2
-
-             strokeStyle "black"
-             font "7pt Chalkboard"
-             textBaseline "top"
-             textAlign "center"
-             strokeText("Blank",16,1)
-             
-             strokeStyle "black"
-             font "7pt Chalkboard"
-             textBaseline "top"
-             textAlign "center"
-             strokeText("Canvas",16,11)             
+             shape (0,0) [(4,6),(-4,6),(3,0),(4,-6),(-4,-6)] "#E8000D" 
+	     shape (8,6) [(-4,6),(3,0),(8,-12),(-3,0),(-2.5,3.75),(-2.5,-3.75),(-3,0)] "#0022B4"
+	     shape (17,3.5) [(-3.5,0),(-8/6,12/6),(3.5+8/6,0)] "#FFC82D" 
+	     shape (17,6.5) [(-5.5,0),(-8/6,12/6),(5.5+8/6,0)] "#FFC82D" 
 
         -- wiki $ txt <- send context $ toDataURL() ; print txt
         wiki $ snapShot context "images/Favicon.png"
