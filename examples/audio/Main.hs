657f86d15140724356654d6e37dbb2ea2d14aa39
Nicholas Shaheed <nshaheed@sulawesi.ittc.ku.edu>
2015-02-05 16:50:59 -0600

    added bounceSound example

--- examples/audio/Main.hs
+++ examples/audio/Main.hs
@@ -2,30 +2,19 @@
 module Main where
 
 import Graphics.Blank
--- import Paths_blank_canvas_examples (getDataDir)
-import Control.Concurrent
-import Network.CGI
+import Paths_blank_canvas_examples (getDataDir)
 
 main :: IO ()
 main = do
-  -- dat <- getDataDir
-  -- blankCanvas 3000 { root = dat } $ \ context -> do
-  blankCanvas 3000 $ \ context -> do    
+  dat <- getDataDir
+  blankCanvas 3000 { root = dat } $ \ context -> do
     send context $ do
-      -- img   <- newImage "images/fan.jpg" --"haskell.png"
-      img   <- newImage "fan.jpg" --"haskell.png"
+      img   <- newImage "images/fan.jpg" 
       drawImage(img,[0,0])
-      florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache.ogg"
-      -- _ <- newAudio "music/sonata.ogg"
+      -- The Audio data type works with both local files and URLs
+      -- florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache.ogg"
+      florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache1.ogg"      
+      _ <- newAudio "music/sonata.ogg"
       fillText("test",800,200)
       play florence
       drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      -- drawImage(img,[70,50])
-      pauseAud florence
