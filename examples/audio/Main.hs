743cc59c64bcade41ba92eff870dba9f3756e62b
Nicholas Shaheed <nshaheed@sulawesi.ittc.ku.edu>
2015-02-05 16:53:54 -0600

    uncommented newAudio line from audio example

--- examples/audio/Main.hs
+++ examples/audio/Main.hs
@@ -12,8 +12,7 @@ main = do
       img   <- newImage "images/fan.jpg" 
       drawImage(img,[0,0])
       -- The Audio data type works with both local files and URLs
-      -- florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache.ogg"
-      florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache1.ogg"      
+      florence <- newAudio "http://upload.wikimedia.org/wikipedia/en/d/df/Florence_Foster_Jenkins_H%C3%B6lle_Rache.ogg"
       _ <- newAudio "music/sonata.ogg"
       fillText("test",800,200)
       play florence
