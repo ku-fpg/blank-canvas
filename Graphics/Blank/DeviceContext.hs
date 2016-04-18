82e0539bd37709483af57cb658e8e3f952c7d037
Andy Gill <andygill@ku.edu>
2016-04-18 10:21:07 -0500

    Scope with syncToFrame

--- Graphics/Blank/DeviceContext.hs
+++ Graphics/Blank/DeviceContext.hs
@@ -53,8 +53,7 @@ devicePixelRatio = ctx_devicePixelRatio
 -- | Internal command to send a message to the canvas.
 sendToCanvas :: DeviceContext -> Instr -> IO ()
 sendToCanvas cxt cmds = do
-    KC.send (theComet cxt) . toStrict . toLazyText $ surround "try{"  "}catch(e){alert('JavaScript Failure: '+e.message);}" <> cmds
-        -- KC.send (theComet cxt) . toStrict . toLazyText $ "try{" <> cmds <> "}catch(e){alert('JavaScript Failure: '+e.message);}"
+    KC.send (theComet cxt) . toStrict . toLazyText $ surround "syncToFrame(function(){"  "});" <> cmds
 
 -- | Wait for any event. Blocks.
 wait :: DeviceContext -> IO Event
