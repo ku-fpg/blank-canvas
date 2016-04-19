fe0c338bbb5253e1f924b73ffed186939dada7c2
Andy Gill <andygill@ku.edu>
2016-04-19 13:04:00 -0500

    Working frame impl.

--- Graphics/Blank/Canvas.hs
+++ Graphics/Blank/Canvas.hs
@@ -200,14 +200,7 @@ instance InstrShow Command where
   showi (Log msg) = "console.log(" <> showiJS msg <> singleton ')'
   showi (Eval cmd) = fromText cmd -- no escaping or interpretation
     -- TODO: Make sure all browsers are supported:
-  showi Frame                        = surround (fromString $ unlines
-                                                  [ "function draw() {"
-                                                  , "  requestAnimationFrame(draw);"
-                                                  ])
-                                                (fromString $ unlines
-                                                  [ "}"
-                                                  , "draw();"
-                                                  ])
+  showi Frame                        = surround "nextFrame(function(){" "})"
 
 -----------------------------------------------------------------------------
 
