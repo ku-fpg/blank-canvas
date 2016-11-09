f435d1d56352cafb83bcc341536a9265ceff8399
Justin Dawson <jtdawso@gmail.com>
2016-11-09 16:59:16 -0600

    added directory creation for diff tool

--- wiki-suite/hack.pl
+++ wiki-suite/hack.pl
@@ -1,3 +1,10 @@
+if ( ! -d "ORIG"){
+  system "git clone http://github.com/ku-fpg/blank-canvas.wiki ORIG";
+}
+if ( ! -d "DIFF"){
+  system "mkdir -p DIFF/images";
+}
+
 foreach (<blank-canvas.wiki/images/*.png>) {
     /blank-canvas.wiki\/(.*)/;
     $nm = $1;
