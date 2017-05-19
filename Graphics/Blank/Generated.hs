3089641b492e2fa0979551a72eb355033e2e388f
Justin Dawson <jtdawso@gmail.com>
2017-05-19 16:06:52 -0500

    latest remote-monad library and stack

--- Graphics/Blank/Generated.hs
+++ Graphics/Blank/Generated.hs
@@ -1,10 +1,11 @@
+{-# LANGUAGE GADTs             #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Graphics.Blank.Generated where
 
-import           Data.Monoid ((<>))
-import           Data.Text.Lazy (fromStrict)
-import qualified Data.Text as ST
+import           Data.Monoid               ((<>))
+import qualified Data.Text                 as ST
+import           Data.Text.Lazy            (fromStrict)
 
 import           Graphics.Blank.Canvas
 import           Graphics.Blank.JavaScript
@@ -16,12 +17,14 @@ import           Graphics.Blank.Instr
 -- import           TextShow (TextShow(..), FromTextShow(..), singleton)
 
 
-instance InstrShow Cmd where
+instance InstrShow a => InstrShow (Prim a) where
     showiPrec _ = showi
     showi (PseudoProcedure f _ _) = showi f
     showi (Method m _)      = showi m
     showi (Command c _)     = showi c
     showi (MethodAudio a _) = showi a
+    showi (Query q _) = showi q
+
 
 instance Show Method where
   showsPrec p = showsPrec p . toString . showi
@@ -33,11 +36,11 @@ instance InstrShow MethodAudio where
   showi (SetCurrentTimeAudio  (audio, time)) = jsAudio audio <> ".currentTime = " <> jsDouble time <> singleton ';'
   showi (SetLoopAudio         (audio, loop)) = jsAudio audio <> ".loop = " <> jsBool loop <> singleton ';'
   showi (SetMutedAudio        (audio, mute)) = jsAudio audio <> ".muted = " <> jsBool mute <> singleton ';'
-  showi (SetPlaybackRateAudio (audio, rate)) = jsAudio audio <> ".playbackRate = " <> jsDouble rate <> singleton ';'  
+  showi (SetPlaybackRateAudio (audio, rate)) = jsAudio audio <> ".playbackRate = " <> jsDouble rate <> singleton ';'
   showi (SetVolumeAudio       (audio, vol))  = jsAudio audio <> ".volume = " <> jsDouble vol <> singleton ';'
 
   -- showi (CurrentTimeAudio audio) = jsAudio audio <> ".currentTime"
-  
+
 instance InstrShow Method where
   showiPrec _ = showi
   showi (Arc (a1,a2,a3,a4,a5,a6)) = "arc("
@@ -109,41 +112,41 @@ instance InstrShow Method where
 -- DSL
 
 -- | @'arc'(x, y, r, sAngle, eAngle, cc)@ creates a circular arc, where
--- 
+--
 -- * @x@ is the x-coordinate of the center of the circle
--- 
+--
 -- * @y@ is the y-coordinate of the center of the circle
--- 
+--
 -- * @r@ is the radius of the circle on which the arc is drawn
--- 
+--
 -- * @sAngle@ is the starting angle (where @0@ at the 3 o'clock position of the circle)
--- 
+--
 -- * @eAngle@ is the ending angle
--- 
+--
 -- * @cc@ is the arc direction, where @True@ indicates counterclockwise and
 --   @False@ indicates clockwise.
 arc :: (Double, Double, Double, Radians, Radians, Bool) -> Canvas ()
-arc = command . Method . Arc
+arc = primitive . Method . Arc
 
 -- | @'arcTo'(x1, y1, x2, y2, r)@ creates an arc between two tangents,
 -- specified by two control points and a radius.
--- 
+--
 -- * @x1@ is the x-coordinate of the first control point
--- 
+--
 -- * @y1@ is the y-coordinate of the first control point
--- 
+--
 -- * @x2@ is the x-coordinate of the second control point
--- 
+--
 -- * @y2@ is the y-coordinate of the second control point
--- 
+--
 -- * @r@ is the arc's radius
 arcTo :: (Double, Double, Double, Double, Double) -> Canvas ()
-arcTo = command . Method . ArcTo
+arcTo = primitive . Method . ArcTo
 
 -- | Begins drawing a new path. This will empty the current list of subpaths.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'beginPath'()
 -- 'moveTo'(20, 20)
@@ -151,44 +154,44 @@ arcTo = command . Method . ArcTo
 -- 'stroke'()
 -- @
 beginPath :: () -> Canvas ()
-beginPath () = command $ Method BeginPath
+beginPath () = primitive $ Method BeginPath
 
 -- | @'bezierCurveTo'(cp1x, cp1y, cp2x, cp2y x, y)@ adds a cubic Bézier curve to the path
 -- (whereas 'quadraticCurveTo' adds a quadratic Bézier curve).
--- 
+--
 -- * @cp1x@ is the x-coordinate of the first control point
--- 
+--
 -- * @cp1y@ is the y-coordinate of the first control point
--- 
+--
 -- * @cp2x@ is the x-coordinate of the second control point
--- 
+--
 -- * @cp2y@ is the y-coordinate of the second control point
--- 
+--
 -- * @x@ is the x-coordinate of the end point
--- 
+--
 -- * @y@ is the y-coordinate of the end point
 bezierCurveTo :: (Double, Double, Double, Double, Double, Double) -> Canvas ()
-bezierCurveTo = command . Method . BezierCurveTo
+bezierCurveTo = primitive . Method . BezierCurveTo
 
 -- | @'clearRect'(x, y, w, h)@ clears all pixels within the rectangle with upper-left
 -- corner @(x, y)@, width @w@, and height @h@ (i.e., sets the pixels to transparent black).
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'fillStyle' \"red\"
 -- 'fillRect'(0, 0, 300, 150)
 -- 'clearRect'(20, 20, 100, 50)
 -- @
 clearRect :: (Double, Double, Double, Double) -> Canvas ()
-clearRect = command . Method . ClearRect
+clearRect = primitive . Method . ClearRect
 
 -- | Turns the path currently being built into the current clipping path.
 -- Anything drawn after 'clip' is called will only be visible if inside the new
--- clipping path. 
+-- clipping path.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'rect'(50, 20, 200, 120)
 -- 'stroke'()
@@ -197,12 +200,12 @@ clearRect = command . Method . ClearRect
 -- 'fillRect'(0, 0, 150, 100)
 -- @
 clip :: () -> Canvas ()
-clip () = command $ Method Clip
+clip () = primitive $ Method Clip
 
 -- | Creates a path from the current point back to the start, to close it.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'beginPath'()
 -- 'moveTo'(20, 20)
@@ -212,68 +215,68 @@ clip () = command $ Method Clip
 -- 'stroke'()
 -- @
 closePath :: () -> Canvas ()
-closePath () = command $ Method ClosePath
+closePath () = primitive $ Method ClosePath
 
 -- | drawImage' takes 2, 4, or 8 'Double' arguments. See 'drawImageAt', 'drawImageSize', and 'drawImageCrop' for variants with exact numbers of arguments.
 drawImage :: Image image => (image,[Double]) -> Canvas ()
-drawImage = command . Method . DrawImage
+drawImage = primitive . Method . DrawImage
 
 -- | Fills the current path with the current 'fillStyle'.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'rect'(10, 10, 100, 100)
 -- 'fill'()
 -- @
 fill :: () -> Canvas ()
-fill () = command $ Method Fill
+fill () = primitive $ Method Fill
 
 -- | @'fillRect'(x, y, w, h)@ draws a filled rectangle with upper-left
 -- corner @(x, y)@, width @w@, and height @h@ using the current 'fillStyle'.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'fillStyle' \"red\"
 -- 'fillRect'(0, 0, 300, 150)
 -- @
 fillRect :: (Double, Double, Double, Double) -> Canvas ()
-fillRect = command . Method . FillRect
+fillRect = primitive . Method . FillRect
 
 -- | Sets the color, gradient, or pattern used to fill a drawing ('black' by default).
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'fillStyle' 'red'
--- 
+--
 -- grd <- 'createLinearGradient'(0, 0, 10, 10)
 -- 'fillStyle' grd
--- 
+--
 -- img <- 'newImage' \"/myImage.jpg\"
 -- pat <- 'createPattern'(img, 'Repeat')
 -- 'fillStyle' pat
 -- @
 fillStyle :: Style style => style -> Canvas ()
-fillStyle = command . Method . FillStyle
+fillStyle = primitive . Method . FillStyle
 
 -- | @'fillText'(t, x, y)@ fills the text @t@ at position @(x, y)@
 -- using the current 'fillStyle'.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'font' \"48px serif\"
 -- 'fillText'(\"Hello, World!\", 50, 100)
 -- @
 fillText :: (ST.Text, Double, Double) -> Canvas ()
-fillText (t, a, b) = command . Method $ FillText (fromStrict t, a, b)
+fillText (t, a, b) = primitive . Method $ FillText (fromStrict t, a, b)
 
 -- | Sets the text context's font properties.
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'font' ('defFont' "Gill Sans Extrabold") { 'fontSize' = 40 # 'pt' }
 -- 'font' ('defFont' 'sansSerif') { 'fontSize' = 80 # 'percent' }
@@ -284,36 +287,36 @@ fillText (t, a, b) = command . Method $ FillText (fromStrict t, a, b)
 -- }
 -- @
 font :: CanvasFont canvasFont => canvasFont -> Canvas ()
-font = command . Method . Font
+font = primitive . Method . Font
 
 -- | Set the alpha value that is applied to shapes before they are drawn onto the canvas.
 globalAlpha :: Alpha -> Canvas ()
-globalAlpha = command . Method . GlobalAlpha
+globalAlpha = primitive . Method . GlobalAlpha
 
 -- | Sets how new shapes should be drawn over existing shapes.
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'globalCompositeOperation' \"source-over\"
 -- 'globalCompositeOperation' \"destination-atop\"
 -- @
 globalCompositeOperation :: ST.Text -> Canvas ()
-globalCompositeOperation = command . Method . GlobalCompositeOperation . fromStrict
+globalCompositeOperation = primitive . Method . GlobalCompositeOperation . fromStrict
 
 -- | Sets the 'LineEndCap' to use when drawing the endpoints of lines.
 lineCap :: LineEndCap -> Canvas ()
-lineCap = command . Method . LineCap
+lineCap = primitive . Method . LineCap
 
 -- | Sets the 'LineJoinCorner' to use when drawing two connected lines.
 lineJoin :: LineJoinCorner -> Canvas ()
-lineJoin = command . Method . LineJoin
+lineJoin = primitive . Method . LineJoin
 
 -- | @'lineTo'(x, y)@ connects the last point in the subpath to the given @(x, y)@
 -- coordinates (without actually drawing it).
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'beginPath'()
 -- 'moveTo'(50, 50)
@@ -321,21 +324,21 @@ lineJoin = command . Method . LineJoin
 -- 'stroke'()
 -- @
 lineTo :: (Double, Double) -> Canvas ()
-lineTo = command . Method . LineTo
+lineTo = primitive . Method . LineTo
 
 -- | Sets the thickness of lines in pixels (@1.0@ by default).
 lineWidth :: Double -> Canvas ()
-lineWidth = command . Method . LineWidth
+lineWidth = primitive . Method . LineWidth
 
 -- | Sets the maximum miter length (@10.0@ by default) to use when the
 -- 'lineWidth' is 'miter'.
 miterLimit :: Double -> Canvas ()
-miterLimit = command . Method . MiterLimit
+miterLimit = primitive . Method . MiterLimit
 
 -- | @'moveTo'(x, y)@ moves the starting point of a new subpath to the given @(x, y)@ coordinates.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'beginPath'()
 -- 'moveTo'(50, 50)
@@ -343,222 +346,222 @@ miterLimit = command . Method . MiterLimit
 -- 'stroke'()
 -- @
 moveTo :: (Double, Double) -> Canvas ()
-moveTo = command . Method . MoveTo
+moveTo = primitive . Method . MoveTo
 
 playAudio :: Audio audio => audio -> Canvas ()
-playAudio = command . MethodAudio . PlayAudio
+playAudio = primitive . MethodAudio . PlayAudio
 
 pauseAudio :: Audio audio => audio -> Canvas ()
-pauseAudio = command . MethodAudio . PauseAudio
+pauseAudio = primitive . MethodAudio . PauseAudio
 
 -- | Sets the current position of the audio value (in seconds).
 setCurrentTimeAudio :: Audio audio => (audio, Double) -> Canvas ()
-setCurrentTimeAudio = command . MethodAudio . SetCurrentTimeAudio
+setCurrentTimeAudio = primitive . MethodAudio . SetCurrentTimeAudio
 
 -- | Set whether the audio value is set to loop (True) or not (False)
 setLoopAudio :: Audio audio => (audio, Bool) -> Canvas ()
-setLoopAudio = command . MethodAudio . SetLoopAudio
+setLoopAudio = primitive . MethodAudio . SetLoopAudio
 
 -- | Set the audio value to muted (True) or unmuted (False)
 setMutedAudio :: Audio audio => (audio, Bool) -> Canvas ()
-setMutedAudio = command . MethodAudio . SetMutedAudio
+setMutedAudio = primitive . MethodAudio . SetMutedAudio
 
 -- | Set the playback value, as a multiplier (2.0 is twice as fast, 0.5 is half speec, -2.0 is backwards, twice as fast)
 setPlaybackRateAudio :: Audio audio => (audio, Double) -> Canvas ()
-setPlaybackRateAudio = command . MethodAudio . SetPlaybackRateAudio
+setPlaybackRateAudio = primitive . MethodAudio . SetPlaybackRateAudio
 
 -- | Adjusts the volume.  Scaled from 0.0 - 1.0, any number outside of this range will result in an error
 setVolumeAudio :: Audio audio => (audio, Double) -> Canvas ()
-setVolumeAudio = command . MethodAudio . SetVolumeAudio
+setVolumeAudio = primitive . MethodAudio . SetVolumeAudio
 
 -- | 'putImageData' takes 2 or 6 'Double' arguments. See `putImageDataAt' and `putImageDataDirty' for variants with exact numbers of arguments.
 putImageData :: (ImageData, [Double]) -> Canvas ()
-putImageData = command . Method . PutImageData
+putImageData = primitive . Method . PutImageData
 
 -- | @'quadraticCurveTo'(cpx, cpy, x, y)@ adds a quadratic Bézier curve to the path
 -- (whereas 'bezierCurveTo' adds a cubic Bézier curve).
--- 
+--
 -- * @cpx@ is the x-coordinate of the control point
--- 
+--
 -- * @cpy@ is the y-coordinate of the control point
--- 
+--
 -- * @x@ is the x-coordinate of the end point
--- 
+--
 -- * @y@ is the y-coordinate of the end point
 quadraticCurveTo :: (Double, Double, Double, Double) -> Canvas ()
-quadraticCurveTo = command . Method . QuadraticCurveTo
+quadraticCurveTo = primitive . Method . QuadraticCurveTo
 
 -- | @'rect'(x, y, w, h)@ creates a rectangle with an upper-left corner at position
 -- @(x, y)@, width @w@, and height @h@ (where width and height are in pixels).
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'rect'(10, 10, 100, 100)
 -- 'fill'()
 -- @
 rect :: (Double, Double, Double, Double) -> Canvas ()
-rect = command . Method . Rect
+rect = primitive . Method . Rect
 
 -- | Restores the most recently saved canvas by popping the top entry off of the
 -- drawing state stack. If there is no state, do nothing.
 restore :: () -> Canvas ()
-restore () = command $ Method Restore
+restore () = primitive $ Method Restore
 
 -- | Applies a rotation transformation to the canvas. When you call functions
 -- such as 'fillRect' after 'rotate', the drawings will be rotated clockwise by
 -- the angle given to 'rotate' (in radians).
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'rotate' ('pi'/2)        -- Rotate the canvas 90°
 -- 'fillRect'(0, 0, 20, 10) -- Draw a 10x20 rectangle
 -- @
 rotate :: Radians -> Canvas ()
-rotate = command . Method . Rotate
+rotate = primitive . Method . Rotate
 
 -- | Saves the entire canvas by pushing the current state onto a stack.
 save :: () -> Canvas ()
-save () = command $ Method Save
+save () = primitive $ Method Save
 
 -- | Applies a scaling transformation to the canvas units, where the first argument
 -- is the percent to scale horizontally, and the second argument is the percent to
 -- scale vertically. By default, one canvas unit is one pixel.
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'scale'(0.5, 0.5)        -- Halve the canvas units
 -- 'fillRect'(0, 0, 20, 20) -- Draw a 10x10 square
 -- 'scale'(-1, 1)           -- Flip the context horizontally
 -- @
 scale :: (Interval, Interval) -> Canvas ()
-scale = command . Method . Scale
+scale = primitive . Method . Scale
 
 -- | Resets the canvas's transformation matrix to the identity matrix,
 -- then calls 'transform' with the given arguments.
 setTransform :: (Double, Double, Double, Double, Double, Double) -> Canvas ()
-setTransform = command . Method . SetTransform
+setTransform = primitive . Method . SetTransform
 
 -- | Sets the blur level for shadows (@0.0@ by default).
 shadowBlur :: Double -> Canvas ()
-shadowBlur = command . Method . ShadowBlur
+shadowBlur = primitive . Method . ShadowBlur
 
 -- | Sets the color used for shadows.
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'shadowColor' 'red'
 -- 'shadowColor' $ 'rgb' 0 255 0
 -- @
 shadowColor :: CanvasColor canvasColor => canvasColor -> Canvas ()
-shadowColor = command . Method . ShadowColor
+shadowColor = primitive . Method . ShadowColor
 
 -- | Sets the horizontal distance that a shadow will be offset (@0.0@ by default).
 shadowOffsetX :: Double -> Canvas ()
-shadowOffsetX = command . Method . ShadowOffsetX
+shadowOffsetX = primitive . Method . ShadowOffsetX
 
 -- | Sets the vertical distance that a shadow will be offset (@0.0@ by default).
 shadowOffsetY :: Double -> Canvas ()
-shadowOffsetY = command . Method . ShadowOffsetY
+shadowOffsetY = primitive . Method . ShadowOffsetY
 
 -- | Draws the current path's strokes with the current 'strokeStyle' ('black' by default).
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'rect'(10, 10, 100, 100)
 -- 'stroke'()
 -- @
 stroke :: () -> Canvas ()
-stroke () = command $ Method Stroke
+stroke () = primitive $ Method Stroke
 
 -- | @'strokeRect'(x, y, w, h)@ draws a rectangle (no fill) with upper-left
 -- corner @(x, y)@, width @w@, and height @h@ using the current 'strokeStyle'.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'strokeStyle' \"red\"
 -- 'strokeRect'(0, 0, 300, 150)
 -- @
 strokeRect :: (Double, Double, Double, Double) -> Canvas ()
-strokeRect = command . Method . StrokeRect
+strokeRect = primitive . Method . StrokeRect
 
 -- | Sets the color, gradient, or pattern used for strokes.
 --
 -- ==== __Examples__
--- 
+--
 -- @
 -- 'strokeStyle' 'red'
--- 
+--
 -- grd <- 'createLinearGradient'(0, 0, 10, 10)
 -- 'strokeStyle' grd
--- 
+--
 -- img <- 'newImage' \"/myImage.jpg\"
 -- pat <- 'createPattern'(img, 'Repeat')
 -- 'strokeStyle' pat
 -- @
 strokeStyle :: Style style => style -> Canvas ()
-strokeStyle = command . Method . StrokeStyle
+strokeStyle = primitive . Method . StrokeStyle
 
 -- | @'strokeText'(t, x, y)@ draws text @t@ (with no fill) at position @(x, y)@
 -- using the current 'strokeStyle'.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'font' \"48px serif\"
 -- 'strokeText'(\"Hello, World!\", 50, 100)
 -- @
 strokeText :: (ST.Text, Double, Double) -> Canvas ()
-strokeText (t, a, b) = command . Method $ StrokeText (fromStrict t, a, b)
+strokeText (t, a, b) = primitive . Method $ StrokeText (fromStrict t, a, b)
 
 -- | Sets the 'TextAnchorAlignment' to use when drawing text.
 textAlign :: TextAnchorAlignment -> Canvas ()
-textAlign = command . Method . TextAlign
+textAlign = primitive . Method . TextAlign
 
 -- | Sets the 'TextBaselineAlignment' to use when drawing text.
 textBaseline :: TextBaselineAlignment -> Canvas ()
-textBaseline = command . Method . TextBaseline
+textBaseline = primitive . Method . TextBaseline
 
 -- | Applies a transformation by multiplying a matrix to the canvas's
 -- current transformation. If @'transform'(a, b, c, d, e, f)@ is called, the matrix
--- 
+--
 -- @
 -- ( a c e )
 -- ( b d f )
 -- ( 0 0 1 )
 -- @
--- 
+--
 -- is multiplied by the current transformation. The parameters are:
--- 
+--
 -- * @a@ is the horizontal scaling
--- 
+--
 -- * @b@ is the horizontal skewing
--- 
+--
 -- * @c@ is the vertical skewing
--- 
+--
 -- * @d@ is the vertical scaling
--- 
+--
 -- * @e@ is the horizontal movement
--- 
+--
 -- * @f@ is the vertical movement
 transform :: (Double, Double, Double, Double, Double, Double) -> Canvas ()
-transform = command . Method . Transform
+transform = primitive . Method . Transform
 
 -- | Applies a translation transformation by remapping the origin (i.e., the (0,0)
 -- position) on the canvas. When you call functions such as 'fillRect' after
 -- 'translate', the values passed to 'translate' are added to the x- and
--- y-coordinate values. 
+-- y-coordinate values.
 --
 -- ==== __Example__
--- 
+--
 -- @
 -- 'translate'(20, 20)
 -- 'fillRect'(0, 0, 40, 40) -- Draw a 40x40 square, starting in position (20, 20)
 -- @
 translate :: (Double, Double) -> Canvas ()
-translate = command . Method . Translate
+translate = primitive . Method . Translate
--- Graphics/Blank/Utils.hs
+++ Graphics/Blank/Utils.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Graphics.Blank.Utils where
 
-import Data.ByteString.Base64  -- Not sure why to use this, vs this *.URL version. This one works, though.
-import qualified Data.ByteString as B
-import Data.Monoid
-import Data.Text(Text)
-import qualified Data.Text as Text
-import Data.Text.Encoding (decodeUtf8, encodeUtf8)
+import qualified Data.ByteString           as B
+import           Data.ByteString.Base64
+import           Data.Monoid
+import           Data.Text                 (Text)
+import qualified Data.Text                 as Text
+import           Data.Text.Encoding        (decodeUtf8, encodeUtf8)
 
-import Graphics.Blank.Canvas
-import Graphics.Blank.Generated
-import Graphics.Blank.JavaScript
+import           Graphics.Blank.Canvas
+import           Graphics.Blank.Generated
+import           Graphics.Blank.JavaScript
 
 
 -- | Clear the screen. Restores the default transformation matrix.
@@ -32,9 +32,9 @@ infixr 0 #
 
 -- | The @#@-operator is the Haskell analog to the @.@-operator
 --   in JavaScript. Example:
--- 
+--
 -- > grd # addColorStop(0, "#8ED6FF");
--- 
+--
 --   This can be seen as equivalent of @grd.addColorStop(0, "#8ED6FF")@.
 (#) :: a -> (a -> b) -> b
 (#) obj act = act obj
@@ -49,7 +49,7 @@ readDataURL mime_type filePath = do
     return $ "data:" <> mime_type <> ";base64," <> decodeUtf8 (encode dat)
 
 -- | Find the MIME type for a data URL.
--- 
+--
 -- > > dataURLMimeType "data:image/png;base64,iVBORw..."
 -- > "image/png"
 dataURLMimeType :: Text -> Text
@@ -73,38 +73,38 @@ writeDataURL fileName
 
 -- | Draws an image onto the canvas at the given x- and y-coordinates.
 drawImageAt :: Image image => (image, Double, Double) -> Canvas ()
-drawImageAt (img, dx, dy) = command . Method $ DrawImage (img, [dx, dy])
+drawImageAt (img, dx, dy) = primitive . Method $ DrawImage (img, [dx, dy])
 
 -- | Acts like 'drawImageAt', but with two extra 'Double' arguments. The third and fourth
 --   'Double's specify the width and height of the image, respectively.
 drawImageSize :: Image image => (image, Double, Double, Double, Double) -> Canvas ()
-drawImageSize (img, dx, dy, dw, dh) = command . Method $ DrawImage (img, [dx, dy, dw, dh])
+drawImageSize (img, dx, dy, dw, dh) = primitive . Method $ DrawImage (img, [dx, dy, dw, dh])
 
 -- | Acts like 'drawImageSize', but with four extra 'Double' arguments before the arguments
 --   of 'drawImageSize'. The first and second 'Double's specify the x- and y-coordinates at
 --   which the image begins to crop. The third and fourth 'Double's specify the width and
 --   height of the cropped image.
--- 
+--
 -- @
 -- 'drawImageCrop' img 0 0 dw dh dx dy dw dh = 'drawImageSize' = dx dy dw dh
 -- @
 drawImageCrop :: Image image => (image, Double, Double, Double, Double, Double, Double, Double, Double) -> Canvas ()
 drawImageCrop (img, sx, sy, sw, sh, dx, dy, dw, dh)
-  = command . Method $ DrawImage (img, [sx, sy, sw, sh, dx, dy, dw, dh])
+  = primitive . Method $ DrawImage (img, [sx, sy, sw, sh, dx, dy, dw, dh])
 
 -- | Writes 'ImageData' to the canvas at the given x- and y-coordinates.
 putImageDataAt :: (ImageData, Double, Double) -> Canvas ()
-putImageDataAt (imgData, dx, dy) = command . Method $ PutImageData (imgData, [dx, dy])
+putImageDataAt (imgData, dx, dy) = primitive . Method $ PutImageData (imgData, [dx, dy])
 
 -- | Acts like 'putImageDataAt', but with four extra 'Double' arguments that specify
 --   which region of the 'ImageData' (the dirty rectangle) should be drawn. The third
 --   and fourth 'Double's specify the dirty rectangle's x- and y- coordinates, and the
 --   fifth and sixth 'Double's specify the dirty rectangle's width and height.
---   
+--
 -- @
 -- 'putImageDataDirty' imgData dx dy 0 0 w h = 'putImageDataAt' imgData dx dy
 --   where (w, h) = case imgData of ImageData w' h' _ -> (w', h')
 -- @
 putImageDataDirty :: (ImageData, Double, Double, Double, Double, Double, Double) -> Canvas ()
 putImageDataDirty (imgData, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight)
-  = command . Method $ PutImageData (imgData, [dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight])
+  = primitive . Method $ PutImageData (imgData, [dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight])
