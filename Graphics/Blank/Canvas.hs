35a70164771ac0b895986318e8e928aa76b90099
Andy Gill <andygill@ku.edu>
2019-12-16 13:16:20 -0600

    DCE

--- Graphics/Blank/Canvas.hs
+++ Graphics/Blank/Canvas.hs
@@ -56,33 +56,7 @@ instance InstrShow TextMetrics
 
 -----------------------------------------------------------------------------
 
--- Method : context.f()
--- Command: f()
--- PseudoProcedure: var asdf_num = ctx.f()
-
-{-
-data Prim :: * -> * where
-  --Cmd
---  Method      :: Method     -> CanvasContext -> Prim ()
---  Command     :: Command     -> CanvasContext -> Prim () -- TODO: Remove this CanvasContext (it's never used)
-  -- TODO: To be merged with 'Method':
---  MethodAudio :: MethodAudio -> CanvasContext -> Prim ()
---  PseudoProcedure  :: InstrShow a => PseudoProcedure a -> CanvasContext -> Prim a
-  --proc
-  Query     :: InstrShow a => Query a     -> CanvasContext -> Prim a
--}
-
-{-
-instance KnownResult Prim where
-  knownResult (Method {}           ) = Just ()
-  knownResult (Command {}         ) = Just ()
-  knownResult (MethodAudio {}     ) = Just ()
-  knownResult (PseudoProcedure {} ) = Just ()
-  knownResult (Query {}           ) = Nothing
--}
-
--- TODO: newtype, after removing InstrShow
-data Canvas a = Canvas 
+newtype Canvas a = Canvas 
         (CanvasContext          -- the context, for the graphic contexts
          -> JS.RemoteMonad a)
 
@@ -103,6 +77,16 @@ instance Monad Canvas where
      Canvas m' -> m' cc
   (>>) = (*>)
 
+instance Semigroup a => Semigroup (Canvas a) where
+  (<>) = liftM2 (<>)
+
+instance Monoid a => Monoid (Canvas a) where
+#if !(MIN_VERSION_base(4,11,0))
+  mappend = liftM2 mappend
+#endif
+  mempty  = return mempty
+
+-- Not sure about this overloading
 class Method r where
   type Context r
   context :: (JSArg (Context r) => Context r -> JS.RemoteMonad ()) -> r
@@ -143,69 +127,6 @@ primitiveQuery f args k = Canvas $ \ cc ->
     Success a -> a) <$> (JS.procedure $ 
        (JS.call f args <> "(" <> showJSB cc <> ")"))
 
-{-
-primitive :: (CanvasContext -> Prim a) -> Canvas a
-primitive f = Canvas $ \ cc -> 
-  case primitive' (f cc) of
-   Canvas g -> g cc
-
-primitive' :: Prim a -> Canvas a
---primitive' (Method m cc) = Canvas $ \ cc ->
---  JS.command $ JS.JavaScript $ toLazyText
---    (jsCanvasContext cc <> singleton '.' <> showi m)
---primitive' (Command cm _cc) = Canvas $ \ cc ->
---  JS.command $ JS.JavaScript $ toLazyText
---    (showi cm)
---primitive' (PseudoProcedure pp cc) = Canvas $ \ cc -> 
---  pseudoProcedureResult pp <$> 
---     (JS.constructor $ JS.JavaScript $ toLazyText 
---          (jsCanvasContext cc <> singleton '.' <> showi pp))
-primitive' (Query q cc) = Canvas $ \ cc ->
-  (\ v -> case parse (parseQueryResult q) v of
-    Error msg -> error msg -- TODO: revisit this fail
-    Success a -> a) <$>
-  (JS.procedure $ JS.JavaScript $ toLazyText 
-  	     (showi q <> "(" <> jsCanvasContext cc <> ")"))
--}
-
-{-
-function :: InstrShow a => (Int -> a) -> PseudoProcedure a -> Canvas a
-function alloc f = do
-  -- Alloc is ignored, and the new number is provided
-  -- by the javascript bridge instead
-  primitive (PseudoProcedure f)
--}
-
--- data Canvas :: * -> * where
---         Method      :: Method                      -> Canvas ()     -- <context>.<method>
---         Command     :: Command                     -> Canvas ()     -- <command>
---         PseudoProcedure    :: TextShow a => PseudoProcedure a    -> Canvas a
---         Query       :: TextShow a => Query a       -> Canvas a
---         With        :: CanvasContext -> Canvas a   -> Canvas a
---         MyContext   ::                                Canvas CanvasContext
---         Bind        :: Canvas a -> (a -> Canvas b) -> Canvas b
---         MethodAudio :: MethodAudio                 -> Canvas ()     -- <audiofile>.<method>
---         Return      :: a                           -> Canvas a
-
--- instance Monad Canvas where
---         return = Return
---         (>>=) = Bind
-
--- instance Applicative Canvas where
---   pure  = return
---   (<*>) = ap
-
--- instance Functor Canvas where
---   fmap f c = c >>= return . f
-
-instance Semigroup a => Semigroup (Canvas a) where
-  (<>) = liftM2 (<>)
-
-instance Monoid a => Monoid (Canvas a) where
-#if !(MIN_VERSION_base(4,11,0))
-  mappend = liftM2 mappend
-#endif
-  mempty  = return mempty
 
 {-
 -- Audio object methods: play(), pause(), setVolume()
@@ -260,65 +181,6 @@ eval :: ST.Text -> Canvas ()
 eval txt = Canvas $ \ _ -> JS.command $ JS.JavaScript $ fromStrict txt
 
 -----------------------------------------------------------------------------
-{-
-data Query :: * -> * where
-        Device               ::                                            Query DeviceAttributes
-        ToDataURL            ::                                            Query Text
-        MeasureText          :: Text                                    -> Query TextMetrics
-        IsPointInPath        :: (Double, Double)                        -> Query Bool
-        NewImage             :: Text                                    -> Query CanvasImage
-        NewAudio             :: Text                                    -> Query CanvasAudio
-        NewCanvas            :: (Int, Int)                              -> Query CanvasContext
-        GetImageData         :: (Double, Double, Double, Double)        -> Query ImageData
-        Cursor               :: CanvasCursor cursor => cursor           -> Query ()
-        Sync                 ::                                            Query ()
-        CurrentTimeAudio     :: CanvasAudio                             -> Query Double
-        -- GetVolumeAudio       :: CanvasAudio                             -> Query Double
-
-instance Show (Query a) where
-  showsPrec p = showsPrec p . I.toString . showi
-
-instance InstrShow (Query a) where
-  showiPrec _ = showi
-  showi Device                       = "Device"
-  showi ToDataURL                    = "ToDataURL"
-  showi (MeasureText txt)            = "MeasureText(" <> jsText txt <> singleton ')'
-  showi (IsPointInPath (x,y))        = "IsPointInPath(" <> jsDouble x <> singleton ','
-                                                        <> jsDouble y <> singleton ')'
-  showi (NewImage url')              = "NewImage(" <> jsText url' <> singleton ')'
-  showi (NewAudio txt)               = "NewAudio(" <> jsText txt  <> singleton ')'
-
-  showi (NewCanvas (x,y))            = "NewCanvas(" <> jsInt x <> singleton ','
-                                                    <> jsInt y <> singleton ')'
-  showi (GetImageData (sx,sy,sw,sh)) = "GetImageData(" <> jsDouble sx <> singleton ','
-                                                       <> jsDouble sy <> singleton ','
-                                                       <> jsDouble sw <> singleton ','
-                                                       <> jsDouble sh <> singleton ')'
-  showi (Cursor cur)                 = "Cursor(" <> jsCanvasCursor cur <> singleton ')'
-  showi Sync                         = "Sync"
-  showi (CurrentTimeAudio aud)       = "CurrentTimeAudio(" <> jsIndexAudio aud <> singleton ')'
-    -- TODO: Find the correct way to implement this:
-  -- showi (GetVolumeAudio   aud)       = "GetVolumeAudio("   <> jsIndexAudio aud <> singleton ')'
-
--- This is how we take our value to bits
-parseQueryResult :: Query a -> Value -> Parser a
-parseQueryResult (Device {}) o                = uncurry3 DeviceAttributes <$> parseJSON o
-parseQueryResult (ToDataURL {}) o             = parseJSON o
-parseQueryResult (MeasureText {}) (Object v)  = TextMetrics <$> v .: "width"
-parseQueryResult (IsPointInPath {}) o         = parseJSON o
-parseQueryResult (NewImage {}) o              = uncurry3 CanvasImage <$> parseJSON o
---parseQueryResult (NewAudio {}) o              = uncurry CanvasAudio <$> parseJSON o
-parseQueryResult (NewCanvas {}) o             = uncurry3 CanvasContext <$> parseJSON o
-parseQueryResult (GetImageData {}) (Object o) = ImageData
-                                           <$> (o .: "width")
-                                           <*> (o .: "height")
-                                           <*> (o .: "data")
-parseQueryResult (Cursor {}) _                = return ()
-parseQueryResult (Sync {}) _                  = return () -- we just accept anything; empty list sent
-parseQueryResult (CurrentTimeAudio {}) o      = parseJSON o
--- parseQueryResult (GetVolumeAudio   {})
-parseQueryResult _ _                          = Fail.fail "no parse in blank-canvas server (internal error)"
--}
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
 uncurry3 f (a,b,c) = f a b c
